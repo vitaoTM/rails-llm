86a2cc56ffa3659e536a8c6bea633aeeb85f3e71
vitor <vitor.grosskopf@gmail.com>
2024-08-06 17:42:09 -0300

    Create scaffold for chats - q_and_a column: set to store an array - history column: ste to receive a json *might be changed latter

---
+++ db/migrate/20240806203148_create_chats.rb
@@ -0,0 +1,11 @@
+class CreateChats < ActiveRecord::Migration[7.1]
+  def change
+    create_table :chats do |t|
+      t.references :user, null: false, foreign_key: true
+      t.json :history
+      t.string :q_and_a, array: true, default: []
+
+      t.timestamps
+    end
+  end
+end
