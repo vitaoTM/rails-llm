214fcd8e0534535df5f17ef0c151107fd1bb4323
vitor <vitor.grosskopf@gmail.com>
2024-08-06 21:06:01 -0300

    Run migrations - Change file application.css to application.scss

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_06_200118) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_06_203148) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "chats", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.json "history"
+    t.string "q_and_a", default: [], array: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_chats_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +35,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_06_200118) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "chats", "users"
 end
