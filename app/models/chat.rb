d219b4f9b1f8aa6f93560ae5913e641e2b4383b3
vitor <vitor.grosskopf@gmail.com>
2024-08-08 18:00:31 -0300

    Refact and add history for chats on chat model - Update cabe.yml

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,34 +1,44 @@
 class Chat < ApplicationRecord
   belongs_to :user
+
   attr_accessor :message
 
   def message=(message)
+    self.history = { 'prompt' => message, 'history' => [] } if history.blank?
+
     messages = [
-      { 'role' => 'system', 'content' => message }
+      { 'role' => 'system', 'content' => history['prompt'] }
     ]
+
     q_and_a.each do |question, answer|
       messages << { 'role' => 'user', 'content' => question }
       messages << { 'role' => 'assistant', 'content' => answer }
     end
-    # respose_raw = client.chat(
-    #   parameters: {
-    #     model: 'gtp-3.5-turbo'
-    #     messages:,
-    #     tremperature: 0.7,
-    #     max_tokens: 500,
-    #     top_p: 1,
-    #     frequency_penalty: 0.0,
-    #     presence_penalty: 0.6
-    #   }
-    # )
+    messages << { 'role' => 'user', 'content' => message } if messages.size > 1
 
-    # Rails.logger.debug response_raw
-    #
-    # self.q_and_a << [message, respose.choises[0].message.content]
+    respose_raw = client.chat(
+      parameters: {
+        model: 'gtp-3.5-turbo',
+        messages:,
+        tremperature: 0.7,
+        max_tokens: 500,
+        top_p: 1,
+        frequency_penalty: 0.0,
+        presence_penalty: 0.6
+      }
+    )
+
+    self.history['history'] << respose_raw
+
+    Rails.logger.debug response_raw
+    respose = JSON.parse(respose_raw.to_json, object_class: OpenStruct)
+
+    self.q_and_a << [message, respose.choises[0].message.content]
   end
 
   private
+
   def client
-    #OpenAI::Clien.new # need to setup .env file with API key
+    OpenAI::Client.new # need to setup .env file with API key
   end
 end
