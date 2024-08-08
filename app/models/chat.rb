baf750fbdf7251a3a81f71c95d4d7a1c15804761
vitor <vitor.grosskopf@gmail.com>
2024-08-08 13:03:23 -0300

    Adds attr accessor to chats model - Needs configuration OpenAi cli - Needs configuration: client parameter - #todo: set up OpenAI API key on  .env file

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,3 +1,34 @@
 class Chat < ApplicationRecord
   belongs_to :user
+  attr_accessor :message
+
+  def message=(message)
+    messages = [
+      { 'role' => 'system', 'content' => message }
+    ]
+    q_and_a.each do |question, answer|
+      messages << { 'role' => 'user', 'content' => question }
+      messages << { 'role' => 'assistant', 'content' => answer }
+    end
+    # respose_raw = client.chat(
+    #   parameters: {
+    #     model: 'gtp-3.5-turbo'
+    #     messages:,
+    #     tremperature: 0.7,
+    #     max_tokens: 500,
+    #     top_p: 1,
+    #     frequency_penalty: 0.0,
+    #     presence_penalty: 0.6
+    #   }
+    # )
+
+    # Rails.logger.debug response_raw
+    #
+    # self.q_and_a << [message, respose.choises[0].message.content]
+  end
+
+  private
+  def client
+    #OpenAI::Clien.new # need to setup .env file with API key
+  end
 end
